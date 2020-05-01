9b6497607a24f8d843b36542f1c0838225f80ae4
malivaibhav15 <malivaibhav15@gmail.com>
2020-05-01 22:52:38 +0530

    ADDED : Method to add the person in the addressbook

--- src/main/java/com/BridgeLabz/services/AddressBook.java
+++ src/main/java/com/BridgeLabz/services/AddressBook.java
@@ -1,42 +1,30 @@
-package com.BridgeLabz;
+package com.BridgeLabz.services;
 
-import jdk.internal.org.objectweb.asm.TypeReference;
-import org.codehaus.jackson.JsonGenerationException;
-import org.codehaus.jackson.JsonParseException;
-import org.codehaus.jackson.map.JsonMappingException;
+import com.BridgeLabz.model.Person;
 import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.type.TypeReference;
 
-import java.io.*;
-import java.util.List;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 
-public class AddressBook {
-    public static void main(String[] args) {
-        try {
-            System.out.println("Welcome to address book");
-            System.out.println("Press 1.To add person");
-            ObjectMapper objectMapper = new ObjectMapper();
-            InputStream inputStream = new FileInputStream(new File("src/main/resources/AddressBook.json"));
-            TypeReference   typeReference = new TypeReference<List<Person>>() {
-            };
-            List<Person> personList = objectMapper.readValues(inputStream, typeReference);
-            for (Person p : personList) {
-                System.out.println("name is" + p.getFirstName() + "lastname" + p.getLastName());
-            }
-            Person person = new Person();
-            person.setFirstName("VAIBHAV");
-            person.setLastName("MALI");
-            objectMapper.writeValue(new File("src/main/resources/AddressBook.json"), person);
-            inputStream.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (JsonParseException e) {
-            e.printStackTrace();
-        } catch (JsonGenerationException e) {
-            e.printStackTrace();
-        } catch (JsonMappingException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+public class AddressBook implements AddressBookInterface {
+
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    public void writeFileData(ArrayList<Person> list, String filePath) throws IOException {
+        objectMapper.writeValue(new File(filePath), list);
+    }
+
+    public ArrayList<Person> readFileData(String filePath) throws IOException {
+        return objectMapper.readValue(new File(filePath), new TypeReference<ArrayList<Person>>() {
+        });
+    }
+
+    @Override
+    public void addPerson(Person person, String pathOfFile) throws IOException {
+        ArrayList<Person> data = readFileData(pathOfFile);
+        data.add(person);
+        writeFileData(data, pathOfFile);
     }
 }
