6051b4f8ea18241540fd9d6307a7a5db8bd28ce0
malivaibhav15 <malivaibhav15@gmail.com>
2020-05-04 00:54:04 +0530

    ADDED : Method declaration

--- src/main/java/com/BridgeLabz/services/AddressBookInterface.java
+++ src/main/java/com/BridgeLabz/services/AddressBookInterface.java
@@ -4,8 +4,16 @@ import com.BridgeLabz.model.Person;
 
 import java.io.IOException;
 
-public interface AddressBookInterface
-{
+public interface AddressBookInterface {
     void addPerson(Person person, String pathOfFile) throws IOException;
-    void  updatePerson(Person person, String pathOfFile) throws IOException;
+
+    void updatePerson(Person person, String pathOfFile) throws IOException;
+
+    void deletePerson(Person person, String pathOfFile) throws IOException;
+
+    void sortByName(String pathOfFile) throws IOException;
+
+    void sortPersonDataByZipCode(String pathOfFile) throws IOException;
+
+    boolean printEntries(String filePath);
 }
