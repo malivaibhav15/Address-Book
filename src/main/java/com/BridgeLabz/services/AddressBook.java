3c6439f4f1afec2f0ddf6088ae9f4e1903fab05a
malivaibhav15 <malivaibhav15@gmail.com>
2020-05-04 18:30:47 +0530

    RWFACTORED : Given proper indentation

--- src/main/java/com/BridgeLabz/services/AddressBook.java
+++ src/main/java/com/BridgeLabz/services/AddressBook.java
@@ -86,6 +86,24 @@ public class AddressBook implements AddressBookInterface {
     }
 
     @Override
+    public boolean newAddressBook(String addressBookName) throws IOException {
+        String filePath = "src/main/resources/" + addressBookName + ".json";
+        File newFile = new File(filePath);
+        if (newFile.createNewFile())
+            return true;
+        return false;
+    }
+
+    @Override
+    public boolean openExistingAddressBook(String addressBookName) {
+        String filePath = "src/main/resources/" + addressBookName + ".json";
+        File openFile = new File(filePath);
+        if (openFile.exists())
+            return true;
+        return false;
+    }
+
+    @Override
     public boolean saveAddressBook(String filePath, ArrayList<Person> data) throws IOException {
         if (data.isEmpty())
             return false;
