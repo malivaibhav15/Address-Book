945f9fd2b36e2fa2f7ff36c456b58f9337aa7a6d
malivaibhav15 <malivaibhav15@gmail.com>
2020-05-04 16:32:46 +0530

    ADDED : Method for opening the file

--- src/main/java/com/BridgeLabz/services/AddressBookMenu.java
+++ src/main/java/com/BridgeLabz/services/AddressBookMenu.java
@@ -1,17 +1,26 @@
-package com.BridgeLabz.InterFace;
+package com.BridgeLabz.services;
+
+import com.BridgeLabz.InterFace.AddressBookMenuInterface;
 
 import java.io.File;
 import java.io.IOException;
 
-public class AddressBookMenu
-{
+public class AddressBookMenu implements AddressBookMenuInterface {
     @Override
     public boolean newAddressBook(String addressBookName) throws IOException {
-        String filePath = "src/main/resources/NewFile.json";
+        String filePath = "src/main/resources/" + addressBookName + ".json";
         File newFile = new File(filePath);
         if (newFile.createNewFile())
             return true;
         return false;
     }
 
+    @Override
+    public boolean openExistingAddressBook(String addressBookName) {
+        String filePath = "src/main/resources/" + addressBookName + ".json";
+        File openFile = new File(filePath);
+        if (openFile.exists())
+            return true;
+        return false;
+    }
 }
